592f214874d8505bc61cfde516a4a9c3749f0222
surajmohan-m <111342002+surajmohan-m@users.noreply.github.com>
2024-05-12 20:04:45 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,8 +10,16 @@ dpkg -i tektoncd-cli-0.36.0_Linux-64bit.deb
 #rpm -i  https://github.com/tektoncd/cli/releases/download/v0.36.0/tektoncd-cli-0.36.0_Linux-64bit.rpm
 
 #Install git clone from hub
-kubectl apply -f  git-clone.yaml
+kubectl apply -f  git-clone.yml
 
 #Install kaniko from hub
-
 kubectl apply -f  kaniko.yml
+
+#install task source list
+kubectl apply -f task_source_list.yml
+
+#install pipeline
+kubectl apply -f pipeline.yml
+
+#create pipeline run
+kubectl create -f pipeline_Run.yml
