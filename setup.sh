0c12a511c1fad2def61f904689e260f050127d46
surajmohan-m <111342002+surajmohan-m@users.noreply.github.com>
2024-05-12 18:52:09 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #Install tekton
-k apply -f https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
+kubectl apply -f https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 
 #Install tkn CLI
 curl -LO https://github.com/tektoncd/cli/releases/download/v0.36.0/tektoncd-cli-0.36.0_Linux-64bit.deb
