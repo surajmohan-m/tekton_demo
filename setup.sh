291e0d054c3d6c5682fe9a329ce49b81a861792e
surajmohan-m <111342002+surajmohan-m@users.noreply.github.com>
2024-05-12 18:50:10 +0530

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#Install tekton
+k apply -f https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
+
+#Install tkn CLI
+curl -LO https://github.com/tektoncd/cli/releases/download/v0.36.0/tektoncd-cli-0.36.0_Linux-64bit.deb
+dpkg -i tektoncd-cli-0.36.0_Linux-64bit.deb
+
+#Or
+
+#rpm -i  https://github.com/tektoncd/cli/releases/download/v0.36.0/tektoncd-cli-0.36.0_Linux-64bit.rpm
+
+#Install git clone from hub
+tkn hub install task git-clone
+
+#Install kaniko from hub
+
+tkn hub install task kaniko
