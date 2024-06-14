a732de3752bd22f0fe0c878d54c9f4f2cf4c11a3
surajmohan-m <111342002+surajmohan-m@users.noreply.github.com>
2024-06-14 20:59:54 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #Install tekton
 kubectl apply -f https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
+
+#Install triggers
 kubectl apply -f https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
 kubectl apply -f https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
+
+#install dashboard
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/dashboard/latest/release.yaml
+kubectl --namespace tekton-pipelines port-forward svc/tekton-dashboard 9097:9097 --address 0.0.0.0 &
 
 #Install tkn CLI
 curl -LO https://github.com/tektoncd/cli/releases/download/v0.36.0/tektoncd-cli-0.36.0_Linux-64bit.deb
